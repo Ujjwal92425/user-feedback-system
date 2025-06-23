52fb571e3bf26ad385446b2bb08a478f788edc7b
Ujjwal Kumar <ujjwalsps54@gmail.com>
2025-06-23 22:46:29 +0530

    Feedback backend (not done)

---
+++ backend/controller/feedbackController.js
@@ -0,0 +1,25 @@
+const Feedback = require("../models/Feedback");
+
+// New  feedback form
+const createFeedback = async (req, res) => {
+  try {
+    const { name, email, feedback, category } = req.body;
+    const newFeedback = new Feedback({ name, email, feedback, category });
+    await newFeedback.save();
+    res.status(201).json({ message: "Feedback saved" });
+  } catch (err) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+// All feedback 
+const getAllFeedback = async (req, res) => {
+  try {
+    const allFeedback = await Feedback.find().sort({ createdAt: -1 });
+    res.json(allFeedback);
+  } catch (err) {
+    res.status(500).json({ error: "Could not fetch feedback" });
+  }
+};
+
+module.exports = { createFeedback, getAllFeedback };
